c67ba7191e09bcc0311f87785050890cce6af9b2
Edgars Žvirblis <98390326+EdgarsZvirblis@users.noreply.github.com>
2023-03-28 12:39:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let diena = Otrdiena
+const input1 = document.querySelector('.js-teksts');
+const plusButton = document.querySelector('.js-plus');
 
-const vards = Toms
+const Add = (skaitlis1, skaitlis2) => {
+    return skaitlis1 + skaitlis2
+}
 
-console.log(...data, diena)
+const Subtract = (skaitlis1, skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Multiply = (skaitlis1, skaitlis2) => {
+    return skaitlis1 * skaitlis2
+}
+
+const Divide = (skaitlis1, skaitlis2) => {
+    return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click', () => {
+    console.log("Mani nospieda")
+})
+
+console.log(input1);
+
+console.log(Add(6,5));
+console.log(Subtract(6,5));
+console.log(Multiply(6,5));
+console.log(Divide(6,5));
